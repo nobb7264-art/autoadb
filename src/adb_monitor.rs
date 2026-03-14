b22055a4d5e8c7ce1bb547516f2b1177e0316cb1
nobb7264-art <nobb7264@gmail.com>
2026-03-14 08:53:14 +0700

    Update adb_monitor.rs

--- src/adb_monitor.rs
+++ src/adb_monitor.rs
@@ -19,7 +19,7 @@
 use crate::void sc_screen_update_content_rect(struct sc_screen *screen) {
     // บังคับให้ขนาดเนื้อหาเท่ากับขนาดหน้าต่างคอมพิวเตอร์ที่ลากไว้
     adb shell wm density
-    adb shell wm size [กว้าง]x[สูง]
+    adb shell wm size
     screen->rect.w = screen->window_width;  // ยืดเต็มความกว้างหน้าต่าง
     screen->rect.h = screen->window_height; // ยืดเต็มความสูงหน้าต่าง
 }
